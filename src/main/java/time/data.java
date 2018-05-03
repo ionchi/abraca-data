65e8622047f509eeb448b7c6d5273f04e9ccd28b
patriziodf <patrizio.difraia@gmail.com>
2018-05-03 14:55:36 +0200

    data class

--- src/main/java/time/data.java
+++ src/main/java/time/data.java
@@ -1,15 +1,20 @@
 package time;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.Date;
 
 public class data extends UDF {
-    public static String unix2StringYear(long time) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(time*1000);
+    public Text evaluate(Text text) {
+        if(text == null) return null;
+        long timestamp = Long.parseLong(text.toString());
+// timestamp*1000 is to convert seconds to milliseconds
+        Date date = new Date(timestamp*1000L);
+// the format of your date
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy");
-        return sdf.format(calendar.getTime()) ;
+        String formattedDate = sdf.format(date);
+        return new Text(formattedDate);
     }
 }
